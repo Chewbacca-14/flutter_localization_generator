2a1629b7b651681721eea3679d7ce598a276d700
Max <bulanovich5@seznam.cz>
2023-12-25 12:52:20 +0100

    fixes

--- bin/filearbgen.dart
+++ bin/filearbgen.dart
@@ -1,6 +1,57 @@
+// gen_en.dart
 import 'dart:io';
+
 import 'package:args/args.dart';
-import 'package:filearbgen/gen_en.dart';
+
+void genEn(String filePath) {
+  Directory current = Directory.current;
+  String currentclear = current
+      .toString()
+      .replaceAll('Directory:', '')
+      .replaceAll('\'', '')
+      .replaceAll(' ', '');
+  String path = '${currentclear}\\lib';
+  var outputFilePath = '$path/en.arb'; // Include the file name
+  print(path);
+
+  try {
+    var file = File(filePath);
+    var contents = file.readAsStringSync();
+
+    var blocks = contents.split('\n');
+
+    var outputFile = File(outputFilePath);
+    var outputSink = outputFile.openWrite();
+
+    outputSink.write('{\n');
+
+    for (var block in blocks) {
+      if (block.trim().isNotEmpty) {
+        var lines = block.split(';');
+        if (lines.length == 3) {
+          var key = lines[0].trim().replaceAll('"', '');
+          var russian = lines[1].trim().replaceAll('"', '');
+          var english = lines[2].trim().replaceAll('"', '');
+
+          outputSink.write(' "$key": "$english",\n');
+          outputSink.write(' "@$key": {\n');
+          outputSink.write('   "description": "$russian"\n');
+          outputSink.write(' },\n');
+        } else {
+          outputSink
+              .write('// Пропущен блок из-за неверного формата: $block\n');
+        }
+      }
+    }
+
+    outputSink.write('}\n');
+    outputSink.close();
+
+    print('Файл en.arb успешно создан: $outputFilePath');
+  } catch (e) {
+    print('Ошибка при чтении/записи файла: $e');
+  }
+}
 
 void main(List<String> arguments) {
   final parser = ArgParser()..addOption('file', abbr: 'f', mandatory: true);
@@ -23,7 +74,9 @@ void main(List<String> arguments) {
 
 void genLanguageFiles(String filePath) {
   genEn(filePath);
-  // try {
+}
+
+// try {
   //   var file = File(filePath);
   //   var contents = file.readAsStringSync();
 
@@ -45,4 +98,3 @@ void genLanguageFiles(String filePath) {
   // } catch (e) {
   //   print('Error reading/writing files: $e');
   // }
-}
